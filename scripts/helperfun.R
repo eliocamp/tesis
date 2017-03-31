dfd5112ec49fd31967849b9b5ab929a7e95a671e
eliocamp <eliocampitelli@gmail.com>
2017-03-31 18:01:55 -0300

    Limpiando código

--- scripts/helperfun.R
+++ scripts/helperfun.R
@@ -1,4 +1,4 @@
-## Archivo con funciones 
+## Archivo con funciones
 
 #Librerías
 library(ggplot2)
@@ -12,49 +12,73 @@ library(directlabels)
 library(akima)
 library(compiler)
 library(RColorBrewer)
-library(rworldmap)
 enableJIT(0)
 
 # Mapa
-mapa <- function(lat = c(-90, 0), fill = NA, res = 1, smooth = 1, pm = 180, ...) {
-  mlat <- min(lat)
-  Mlat <- max(lat)
-  # m <- getMap(resolution = "coarse")
-  data(coastsCoarse)
-  m <- coastsCoarse
-  m <- as.data.table(fortify(m))
-  m[, group := as.numeric(group)]
-  m[, id := as.numeric(id)]
-  m2 <- copy(m)
-  m2[, long := long + 360]
-  m2[, group := group + max(group) + 1]
+BuildMap <- function(res = 1, smooth = 1, pm = 180,
+                     countries = FALSE, ...) {
+    # Caraga datos de mapas y cambia el meridiano principal, suaviza y cambia
+    # resolución para plotear más rápido.
+    # Entra:
+    #   res: resolución (cuantos puntos se eliminan entre los que quedan)
+    #   smooth: factor de suavizado (ventana del promedio corrido)
+    #   pm: longitud del meridiano central
+    #   countries: ¿datos a nivel país?
+    # Sale:
+    #   un data.table con las coordenadas de cada polígono y su grupo
+    library(data.table)
+    if (countries) {
+        library(rworldxtra)
+        data("countriesLow")
+        m <- countriesLow
+    } else {
+        library(rworldmap)
+        data(coastsCoarse)
+        m <- coastsCoarse
+    }
 
-  m <- rbind(m, m2)
-  cut <- max(smooth, res)
-  notsmooth <- m[, .N, by = group][N < cut + 4, group]
-  # m <- m[long < 0 & long > 360, `:=`(long = NA, lat = NA)]
-  m[, MO := max(order), by = group]
-  m[!(group %in% notsmooth) & order != 1 & order != MO, 
-    `:=`(long = zoo::rollmean(long, smooth, fill = "extend"),
-         lat = zoo::rollmean(lat, smooth, fill = "extend")), 
-    by = group]
-  
-  m[, keep := c(1, rep(0, res)), by = group]
-  m <- m[order == 1 | order == MO | keep == 1, .SD, by = group]
-  m <- m[!(group %in% notsmooth)]
-  m <- m[long >= pm - 180 & long <= 180 + pm]
-  m <- m[lat >= mlat & lat <= Mlat]
+    m <- as.data.table(fortify(m))
+    m[, group := as.numeric(group)]
+    m[, id := as.numeric(id)]
 
-  return(geom_path(data = m, aes(long, lat, group = group), 
-                      color="black", 
-                      inherit.aes=F, size = 0.2))
+    # Cambio el prime meridian.
+    m2 <- copy(m)
+    m2[, long := long + 360]
+    m2[, group := group + max(group) + 1]
+    m <- rbind(m, m2)
+    m <- m[long >= pm - 180 & long <= 180 + pm]
+
+    # Suavizo.
+    cut <- max(smooth, res)
+    notsmooth <- m[, .N, by = group][N < cut + 4, group]
+    m <- m[!(group %in% notsmooth)]   # saco los grupos muy chicos
+    m[, MO := max(order), by = group]
+    m[order != 1 & order != MO,
+      `:=`(long = zoo::rollmean(long, smooth, fill = "extend"),
+           lat = zoo::rollmean(lat, smooth, fill = "extend")),
+      by = group]
+
+    # Bajo la resolución.
+    m[, keep := c(1, rep(0, res)), by = group]
+    m <- m[order == 1 | order == MO | keep == 1, .SD, by = group]
+
+    return(m)
 }
 
-# Repite la longitud, para cerrar los contornos en un plot polar
-rep_lon <- function(x) {
-  border <- x[lon == min(lon), ]
-  border$lon <- 360
-  rbind(x, border)
+geom_map2 <- function(map) {
+    # Un geom_path con defaults copados para agregar mapas
+    g <- geom_path(data = map, aes(long, lat, group = group),
+               color = "black",
+               inherit.aes = F, size = 0.2)
+    return(g)
+}
+
+
+RepeatLon <- function(x) {
+    # Repite la longitud, para cerrar los contornos en un plot polar
+    border <- x[lon == min(lon), ]
+    border$lon <- 360
+    rbind(x, border)
 }
 
 # lm2 <- function(...) {
@@ -62,245 +86,266 @@ rep_lon <- function(x) {
 # }
 # lm_m <- memoise(lm2)
 # Función para calcular onda estacionaria y su amplitud
-qs_fit <- function(x, n = 1) {
-  f <- fft(x)
-  f <- f/length(f)
-  amp <- Mod(f)*2
-  fase <- Arg(f)
-  r <- amp^2/(2*sd(x)^2)
-  n <- n + 1
-  
-  cols <- c("Amplitud", "Fase", "R.sqr", "QS")
-  ret <- list(amp[n], fase[n], r[n], n - 1)
-  names(ret) <- cols
-  return(ret)
+FitQsWave <- function(x, n = 1) {
+    # Calcula los parámetros de números de onda. Se lleva bien con n vector
+    # y data.table
+    # Entra:
+    #   x: vector de entrada
+    #   n: vector con los números de onda a calcular
+    # Sale:
+    #   una lista con la amplitud, la fase, la varianza explicada y el número
+    #   de onda.
+    f <- fft(x)
+    f <- f/length(f)
+    amp <- Mod(f)*2
+    fase <- Arg(f)
+    r <- amp^2/(2*sd(x)^2)
+    n <- n + 1
+
+    cols <- c("amplitude", "phase", "r2", "k")
+    ret <- list(amp[n], fase[n], r[n], n - 1)
+    names(ret) <- cols
+    return(ret)
 }
 
 # qs_fit_m <- memoise(qs_fit)
 
 
-ncread <- function(file, vars, dims = c("lon", "lat", "lev", "time")){
-  ncfile <- nc_open(file)
-  dimensions <- list()
-  for (i in 1:length(dims)){
-    dimensions[[dims[i]]] <- ncvar_get(ncfile, dims[i])
-  }
-  # 
-  # if (!is.null(lat)) latvar <- ncvar_get(ncfile, "lat")
-  # if (!is.null(lon)) lonvar <- ncvar_get(ncfile, "lon")
-  # if (!is.null(time)) timevar <- ncvar_get(ncfile, "time")
-  # if (!is.null(lev)) levvar <- ncvar_get(ncfile, "lev")
-  
-  var1 <- ncvar_get(ncfile, vars[1])
-  
-  # dimnames(var1) <- list(lon, lat, lev, time)
-  dimnames(var1) <- dimensions
-  # nc <- as.data.table(melt(var1, varnames = c("lon", "lat", "lev", "time"), value.name = vars[1]))
-  nc <- as.data.table(melt(var1, varnames = names(dimensions), value.name = vars[1]))
-  nc[, c(vars[-1]) := lapply(vars[-1], ncvar_get, nc = ncfile)]
-  nc_close(ncfile)
-  return(nc)
+ReadNetCDF <- function(file, vars) {
+    # Usa la librería netcdf para leer archivos y organiza todo en un data.table
+    # Entra:
+    #   file: la ruta del archivo
+    #   vars: las variables a leer
+    # Sale:
+    #   un elemento de clase data.table con las variables en cada columna
+
+    library(ncdf4)
+    library(data.table)
+    ncfile <- nc_open(file)
+
+    # Leo las dimensiones
+    dimensions <- list()
+    for (i in seq_along(dims)) {
+        dimensions[[dims[i]]] <- ncvar_get(ncfile, dims[i])
+    }
+
+    # Leo la primera variable para luego hacer melt y obtener el data.table
+    # al que luego le agrego las otras variables
+    var1 <- ncvar_get(ncfile, vars[1])
+    dimnames(var1) <- dimensions
+    nc <- melt(var1, varnames = names(dimensions), value.name = vars[1])
+    setDT(nc)
+    nc[, c(vars[-1]) := lapply(vars[-1], ncvar_get, nc = ncfile)]    # otras variables
+
+    # Dejemos todo prolijo antes de cerrar.
+    nc_close(ncfile)
+    return(nc)
 }
 
 
 # "Similar"
 `%~%` <- function(x, target) {
-  x <- abs(x - target)
-  return(x == min(x))
+    x <- abs(x - target)
+    return(x == min(x))
 }
 
 `%b%` <- function(x, limits) {
-    return(x >= limits[1] & x <= limits[2])
+    # Operador "between"
+    return(x >= min(limits) & x <= max(limits))
 }
 
 
 
 # Salteado
 jumpby <- function(vector, by) {
-    vector[seq(1, length(vector), by = by)] 
+    vector[seq(1, length(vector), by = by)]
 }
 
 # Repetir vector en negativo
 fill_neg <- function(vector) {
-  c(-vector[order(-vector)], vector)
+    c(-vector[order(-vector)], vector)
 }
 
 
 library("scales")
 reverselog_trans <- function(base = 10) {
-  trans <- function(x) -log(x, base)
-  inv <- function(x) base^(-x)
-  trans_new(paste0("reverselog-", format(base)), trans, inv, 
-            log_breaks(base = base), 
-            domain = c(1e-100, Inf))
+    trans <- function(x) -log(x, base)
+    inv <- function(x) base^(-x)
+    trans_new(paste0("reverselog-", format(base)), trans, inv,
+              log_breaks(base = base),
+              domain = c(1e-100, Inf))
 }
 
 # Nombres de los meses en español
-month.abb <- c("Ene", "Feb", "Mar", "Abr", "May", "Jun", 
+month.abb <- c("Ene", "Feb", "Mar", "Abr", "May", "Jun",
                "Jul", "Ago", "Sep", "Oct", "Nov", "Dic")
 names(month.abb) <- as.character(1:12)
 
 
 
-# Asigna estaciones del año 
+# Asigna estaciones del año
 asign_season <- function(month) {
-  seasons <- c("Verano", "Verano", rep(c("Otoño", "Invierno", "Primavera"), each = 3), "Verano")
-  return(factor(seasons[month], levels = c("Verano", "Otoño", "Invierno", "Primavera")))
+    seasons <- c("Verano", "Verano", rep(c("Otoño", "Invierno", "Primavera"), each = 3), "Verano")
+    return(factor(seasons[month], levels = c("Verano", "Otoño", "Invierno", "Primavera")))
 }
 
 # Para hacer anomalías.
-anom <- function(x) {
-  as.numeric(scale(x, scale = F))
+Anomaly <- function(x) {
+    as.numeric(scale(x, scale = F))
 }
 
 # Para interpolación en data table
 interp.dt <- function(...){
-  int <- interp(...)
-  r <- with(int, { 
-    grid <- expand.grid(x, y)
-    r <- list(grid[,1], grid[, 2], c(z))
-    return(r)
-  })
+    int <- interp(...)
+    r <- with(int, {
+        grid <- expand.grid(x, y)
+        r <- list(grid[,1], grid[, 2], c(z))
+        return(r)
+    })
 }
 
 # Función que hace autocorrelograma y su test según Anderson o large lag.
 acf.sig <- function(x, lag.max=0.3*length(x), alpha = 0.05, method=c("anderson","large.lag", "salas"), sided="one") {
-  autocor <- acf(x, lag.max=lag.max, plot = F)$acf
-  N <- length(x)
-  e <- -1/(N-1)
-  if (method[1]=="anderson"){
-    var <- (N-2)/(N-1)^2
-  } else if (method[1]=="large.lag"){
-    var <- vector()
-    for (i in 1:length(autocor)){
-      v <- ifelse(i==1,1/N, 1/N * (1+2*sum(autocor[1:i-1]^2)))
-      var<- c(var, v)
+    autocor <- acf(x, lag.max=lag.max, plot = F)$acf
+    N <- length(x)
+    e <- -1/(N-1)
+    if (method[1]=="anderson"){
+        var <- (N-2)/(N-1)^2
+    } else if (method[1]=="large.lag"){
+        var <- vector()
+        for (i in 1:length(autocor)){
+            v <- ifelse(i==1,1/N, 1/N * (1+2*sum(autocor[1:i-1]^2)))
+            var<- c(var, v)
+        }
+    } else if (method[1]=="salas"){
+        var <- vector()
+
+        for (i in 1:length(autocor)){
+            v <- (N-1-i)/(N-i)^2
+            var<- c(var, v)
+            e <- -1/(N-i)
+        }
     }
-  } else if (method[1]=="salas"){
-    var <- vector()
-    
-    for (i in 1:length(autocor)){
-      v <- (N-1-i)/(N-i)^2
-      var<- c(var, v)
-      e <- -1/(N-i)
+    if (sided=="one"){
+        a <- alpha
+        q <- qnorm(a, lower.tail=F)
+        sigupp <- e + sqrt(var)*q
+        ret <- data.frame(lag=0:lag.max, acf=autocor, sig.cut=sigupp)
+    } else if (sided == "two"){
+        a <- alpha/2
+        q <- qnorm(a, lower.tail=F)
+        sigupp <- e+sqrt(var)*q
+        siginf <- e-sqrt(var)*q
+        ret <- data.frame(lag=0:lag.max, acf=autocor, upp.sig.cut=sigupp, low.sig.cut=siginf)
     }
-  }
-  if (sided=="one"){
-    a <- alpha
-    q <- qnorm(a, lower.tail=F)
-    sigupp <- e + sqrt(var)*q
-    ret <- data.frame(lag=0:lag.max, acf=autocor, sig.cut=sigupp)
-  } else if (sided == "two"){
-    a <- alpha/2
-    q <- qnorm(a, lower.tail=F)
-    sigupp <- e+sqrt(var)*q
-    siginf <- e-sqrt(var)*q
-    ret <- data.frame(lag=0:lag.max, acf=autocor, upp.sig.cut=sigupp, low.sig.cut=siginf)
-  }
-  ret
+    ret
 }
 
 
 
 # Convierte la salida de la función fft en un formato
-# legible por humanos. 
+# legible por humanos.
 
 convert.fft <- function(cs, sample.rate=1, full=T) {
-  distance.center <- function(c) Mod(c)
-  angle <- function(c) Arg(c)
-  is.even <- function(x) ceiling(x/2) == x/2
-  N <- length(cs)
-  if (full==T){
-    nyq <- ifelse(is.even(N), N/2+1, (N+1)/2)
-    cs <- cs[2:nyq]
-  }
-  NP <- length(cs)
-  cs <- cs / N # normalize
-  
-  df <- data.frame(cycle    = 1:(NP),
-                   freq     = 1:(NP) * sample.rate / N,
-                   per      = N/(1:(NP) * sample.rate),
-                   ampl = sapply(cs, Mod),
-                   delay    = sapply(cs, angle),
-                   spect    = sqrt(sapply(cs, Mod)),
-                   comp = cs)
-  
-  non.unique <- ifelse(is.even(NP), NP-1, NP)
-  df$ampl [1:non.unique] <- df$ampl[1:non.unique]*2
-  df
+    distance.center <- function(c) Mod(c)
+    angle <- function(c) Arg(c)
+    is.even <- function(x) ceiling(x/2) == x/2
+    N <- length(cs)
+    if (full==T){
+        nyq <- ifelse(is.even(N), N/2+1, (N+1)/2)
+        cs <- cs[2:nyq]
+    }
+    NP <- length(cs)
+    cs <- cs / N # normalize
+
+    df <- data.frame(cycle    = 1:(NP),
+                     freq     = 1:(NP) * sample.rate / N,
+                     per      = N/(1:(NP) * sample.rate),
+                     ampl = sapply(cs, Mod),
+                     delay    = sapply(cs, angle),
+                     spect    = sqrt(sapply(cs, Mod)),
+                     comp = cs)
+
+    non.unique <- ifelse(is.even(NP), NP-1, NP)
+    df$ampl [1:non.unique] <- df$ampl[1:non.unique]*2
+    df
 }
 
 # Abreviatura para as.data.table
 as.dt <- function(...) {
-  as.data.table(...)
+    as.data.table(...)
 }
 
 
 
 # Función para agregar etiquetas a contornos
-# Uso: primero crear un objeto ggplot con todo el plot 
+# Uso: primero crear un objeto ggplot con todo el plot
 # luego agregar un geom_text con data = make_labels(g)
 
 minvar <- function (x, y){
-  counts = length(x)
-  xdiffs = diff(x)
-  ydiffs = diff(y)
-  avgGradient = ydiffs/xdiffs
-  squareSum = avgGradient * avgGradient
-  variance = (squareSum - (avgGradient * avgGradient) / counts / counts)
-  variance = c(9999999, variance) #99999 pads this so the length is same as original and the first values are not selected
-  return(variance == min(variance))
+    counts = length(x)
+    xdiffs = diff(x)
+    ydiffs = diff(y)
+    avgGradient = ydiffs/xdiffs
+    squareSum = avgGradient * avgGradient
+    variance = (squareSum - (avgGradient * avgGradient) / counts / counts)
+    variance = c(9999999, variance) #99999 pads this so the length is same as original and the first values are not selected
+    return(variance == min(variance))
 }
 
 make_labels <- function(g, step = 2) {
-  tmp3 <- as.data.table(ggplot_build(g)$data[[1]])
-  tmp3[, var := minvar(x, y), by = group]
-  return(tmp3[var == T][seq(1, .N, by = step)])
+    tmp3 <- as.data.table(ggplot_build(g)$data[[1]])
+    tmp3[, var := minvar(x, y), by = group]
+    return(tmp3[var == T][seq(1, .N, by = step)])
 }
 
 
 factor2cols <- function(x, column, factors) {
-  column <- deparse(substitute(column))
-  for(h in factors){
-    x[, (h) := ifelse(get(column) == h, 1, 0)]
-  }
-  x[is.na(get(column)), (factors) := 0]
+    column <- deparse(substitute(column))
+    for(h in factors){
+        x[, (h) := ifelse(get(column) == h, 1, 0)]
+    }
+    x[is.na(get(column)), (factors) := 0]
 }
 
 # convierte una fecha en formato AAAA-MM-DD en mes con factor y ordenado según estaciones
-date_month2factor <- function(x) { 
-  factor(as.numeric(stringi::stri_sub(x, 6, 7)), levels = c(12, 1:11), ordered = T)
+date_month2factor <- function(x) {
+    factor(as.numeric(stringi::stri_sub(x, 6, 7)), levels = c(12, 1:11), ordered = T)
 }
 
 # genera coeficiente y error estandar de regresión lineal. Para uso en data.table
 lmcoef <- function(formula) {
-  a <- summary(fastLm(formula = formula))
-  list(estimate  = a$coefficients[2, 1],
-       se        = a$coefficients[2, 2])
+    a <- summary(fastLm(formula = formula))
+    list(estimate  = a$coefficients[2, 1],
+         se        = a$coefficients[2, 2])
 }
 
 
 
 panel_timeseries <- function(g, x, n = 2, xlab = "x", ylab = "y") {
-  M <- max(x)
-  m <- min(x)
-  step <- (M - m)/n
-  g <- g + labs(x="", y="") + theme(axis.title = element_blank())
-  library(grid)
-  library(gridExtra)
-  plots <- list()
-  
-  for (i in 1:n) {
-    if (i == 1) {
-      pl <- ggplot_gtable(ggplot_build(g))
-      leg <- which(sapply(pl$grobs, function(x) x$name) == "guide-box")
-      legend <- pl$grobs[[leg]]
+    M <- max(x)
+    m <- min(x)
+    step <- (M - m)/n
+    g <- g + labs(x="", y="") + theme(axis.title = element_blank())
+    library(grid)
+    library(gridExtra)
+    plots <- list()
+
+    for (i in 1:n) {
+        if (i == 1) {
+            pl <- ggplot_gtable(ggplot_build(g))
+            leg <- which(sapply(pl$grobs, function(x) x$name) == "guide-box")
+            legend <- pl$grobs[[leg]]
+        }
+        pl <- g + coord_cartesian(xlim = as.Date(c(m + step*(i-1), m + i*step))) +
+            theme(axis.title = element_blank()) + guides(color = FALSE)
+        pl <- ggplot_gtable(ggplot_build(pl))
+        plots[[i]] <- pl
     }
-    pl <- g + coord_cartesian(xlim = as.Date(c(m + step*(i-1), m + i*step))) +
-      theme(axis.title = element_blank()) + guides(color = FALSE)
-    pl <- ggplot_gtable(ggplot_build(pl))
-    plots[[i]] <- pl 
-  }
-  plots[[n + 1]] <- legend
-  grid.arrange(grobs = plots, ncol = 1, heights = c(rep(10, n), 2), bottom = xlab, left = ylab)
+    plots[[n + 1]] <- legend
+    grid.arrange(grobs = plots, ncol = 1, heights = c(rep(10, n), 2), bottom = xlab, left = ylab)
 }
+
+
+theme_elio <- theme_minimal() +
+    theme(legend.position = "bottom")
--- scripts/tidy_speedy.R
+++ scripts/tidy_speedy.R
@@ -1,29 +1,24 @@
-# Pasa Speedy a .csv
+# Lee y le da formato a la salida de Speedy
+
 library(data.table)
 library(lubridate)
 library(fields)
 library(ncdf4)
-source("helperfun.R")
+source("scripts/helperfun.R")
 
-# Valores medios
+# Valores medios.
 file <- "DATA/SPEEDY/attm.nc"
 ncfile <- nc_open(file)
 vars <- c("gh", "u", "v", "psi", "temp")
-speedy<- ncread(file, vars)
+speedy <- ReadNetCDF(file, vars)
 
-# Selecciono sólo el sector que me importa.
+# Selecciono sólo el sector que me importa y le pongo formato a la fecha.
 speedy <- speedy[lat < 0]
-speedy[, time :=as.Date("1985-01-01 00:00:00") + time/24]
-speedy[, month := factor(month.abb[data.table::month(time)], levels = month.abb)]
-speedy[, year := year(time)]
-# setindex(speedy, lon, lat, lev, month, year)
+speedy[, time := as.Date("1985-01-01 00:00:00") + time/24]
+setnames(speedy, "time", "date")
+setnames(speedy, "temp", "t")
+setindex(speedy, lon, lat, lev, date)
 
+# Guardo una versión en binario y otra en csv
+save(speedy, file = "DATA/SPEEDY/speedy.Rda")
 fwrite(speedy, "DATA/SPEEDY/todo_speedy.csv")
-
-
-m_speedy <- speedy[, lapply(.SD, mean),
-                   by = .(lon, lat, lev, month),
-                   .SDcols = vars]
-
-fwrite(m_speedy, "DATA/SPEEDY/todo_m_speedy.csv")
-remove(speedy, m_speedy)
