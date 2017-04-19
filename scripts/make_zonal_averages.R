1f3bbdf7e264d730f2317605e55fd731dfc0376a
eliocamp <eliocampitelli@gmail.com>
2017-04-18 21:44:38 -0300

    mejor documentación

--- scripts/make_zonal_averages.R
+++ scripts/make_zonal_averages.R
@@ -2,9 +2,26 @@
 
 library(ncdf4)
 library(data.table)    # soy un adicto a data.table
-source("scripts/readctl.R")    # ¿y este script de dónde salió? ¿lo escribí yo?
+source("scripts/ReadCtl.R")    # ¿y este script de dónde salió? ¿lo escribí yo?
 
-dir <- "speedy_ver41_ws_clim_var_2015/data/bc/t30zonal/clim/"
+MakeZonal <- function(descriptor) {
+    field <- ReadCtl(descriptor, hd.rm = T)
+    dims <- dim(field)
+    field <- as.data.table(melt(field))
+    colnames(field) <- c("lon", "lat", "lev", "var", "month", "value")
+
+    field[, value.z := mean(value[value > -999]), by = .(lat, lev, var, month)]
+    field[value < -99, value.z := min(value)]
+    field[, value := NULL]
+    field <- c(array(field$value.z,  dim = dims))    # creo que no tiene sentido
+
+    # Tengo que agregarle los headers.
+    outfield <- c(ReadCtl(descriptor = descriptor, hd.rm = F))
+    outfield[outfield != outfield[1]] <- field
+    return(outfield)
+}
+
+dir <- "speedy_ver41_ws_clim_var_2015/data/bc/t30_zonal/clim/"
 descriptor <- paste0(dir, "sst_7908clim.t30.sea.ctl")
 outfile <- paste0(dir, "sst_7908clim.t30.sea.grd")
 
